11c1b1a55a3e0689bdccc035e2c369de4950ec56
varadpundlik <varadpundlik@gmail.com>
2022-07-26 18:01:37 +0530

    v4.0

--- script.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  var x = document.getElementById("menu");
-  if (x.className === "menu") {
-    x.className += " responsive";
-  } else {
-    x.className = "menu";
-  }
-}
